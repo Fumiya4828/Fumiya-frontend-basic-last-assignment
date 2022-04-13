89aae66140c8e581fccd8f45be4751f0584d0948
Fumiya Abe <kkjpq9683@gmail.com>
2022-04-13 17:28:56 +0900

    NewsAndEventsMoreを作製しました

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,6 @@
+import Molecules_More from "../components/molecules/news-and-events-more/news-and-events-more.html";
+import "../components/molecules/news-and-events-more/news-and-events-more.scss";
+
+export default { title: "Molecules/More" }
+
+export const More = () => Molecules_More;
