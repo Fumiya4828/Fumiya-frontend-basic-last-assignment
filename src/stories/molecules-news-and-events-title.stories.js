a5aba98e5eab91e2601a004ddd9e84483dc6806b
Fumiya Abe <kkjpq9683@gmail.com>
2022-04-13 15:37:25 +0900

    Molecules/news-and-events-titleを作製しました

---
+++ src/stories/molecules-news-and-events-title.stories.js
@@ -0,0 +1,6 @@
+import Molecules_title from "../components/molecules/news-and-events-title/news-and-events-title.html";
+import "../components/molecules/news-and-events-title/news-and-events-title.scss";
+
+export default { title: "Molecules/news-and-events-title" }
+
+export const Molecules = () => Molecules_title;
