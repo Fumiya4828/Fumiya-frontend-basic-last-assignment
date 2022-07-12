eff376dee543364839f70b3ad568206348ffbf03
Fumiya Abe <kkjpq9683@gmail.com>
2022-07-12 15:23:23 +0900

    Organism/Newsのファイル作成

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import Organisms_News from "../components/molecules/organisms/news/news.html";
+import "../components/molecules/organisms/news/news.scss";
+
+export default { title: "Organisms/News" }
+
+export const Organisms = () => Organisms_News;
